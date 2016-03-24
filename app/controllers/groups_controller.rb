4fd62162ae40055393a13386c78f68ad939b3c55
Wen <sunzibinfa@gmail.com>
2016-03-24 19:54:47 +0800

    post各項action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -34,6 +34,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324062604) do
+ActiveRecord::Schema.define(version: 20160324071420) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.string   "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
