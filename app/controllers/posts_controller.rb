2980d0965e742025a9871aab09a392ae8ffc162d
Wen <sunzibinfa@gmail.com>
2016-03-26 22:28:37 +0800

    post的創造者才能進行部分actions且edit與delete按鈕才會顯示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     @post = @group.posts.build(post_params)
+    @post.author = current_user
 
     if @post.save
       redirect_to group_path(@group)
@@ -19,12 +20,12 @@ class PostsController < ApplicationController
 
   def edit
     @group = Group.find(params[:group_id])
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
     @group = Group.find(params[:group_id])
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
 
     if @post.update(post_params)
       redirect_to group_path(@group)
@@ -35,7 +36,7 @@ class PostsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:group_id])
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     @post.destroy
     redirect_to group_path(@group)
   end
