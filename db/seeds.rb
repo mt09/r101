96300d996ec7ffaae2ae3d52611f7f472844d3d2
Wen <sunzibinfa@gmail.com>
2016-03-26 21:09:19 +0800

    strong parameter與devise整合, seed設定一組user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+o = User.new
+o.email = "w@w.com"
+o.name = "ww"
+o.password = "qwerasdf"
+o.password_confirmation = "qwerasdf"
+o.save
