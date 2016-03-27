6be240d37d8de93d2da239011d71d9f8b8cd8f5f
Wen <AleJet@Wens-MacBook-Pro.local>
2016-03-27 12:52:22 +0800

    My Posts, 在My Posts頁面刪除文章後仍回到My Post頁面, Group刪除後Post也跟著刪除

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :groups
+    resources :posts
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
